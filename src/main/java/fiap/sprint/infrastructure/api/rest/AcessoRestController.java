99957540309f08f228050624064554bc35065013
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-10-25 20:39:38 -0300

    Rest Controller de Acesso terminado

--- src/main/java/fiap/sprint/infrastructure/api/rest/AcessoRestController.java
+++ src/main/java/fiap/sprint/infrastructure/api/rest/AcessoRestController.java
@@ -1,17 +1,74 @@
 package fiap.sprint.infrastructure.api.rest;
 
+import fiap.sprint.domain.model.Acesso;
+import fiap.sprint.dto.input.AcessoInputDto;
+import fiap.sprint.dto.output.AcessoOutputDto;
 import fiap.sprint.interfaces.AcessoController;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+import fiap.sprint.mapper.AcessoMapper;
 
 @Path("/Acesso")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AcessoRestController {
     private final AcessoController acessoController;
-    public AcessoRestController(AcessoController acessoController){
+    public AcessoRestController(AcessoController acessoController) {
         this.acessoController = acessoController;
     }
+
+    @POST
+    public Response create(AcessoInputDto dto) {
+        try {
+            Acesso acessoCriado = acessoController.createAcesso(dto.getIdPagina(), dto.getIdUsuario());
+            AcessoOutputDto acessoSaida = AcessoMapper.toDto(acessoCriado);
+            return Response.status(Response.Status.CREATED).entity(acessoSaida).build();
+        } catch (RuntimeException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response update(@PathParam("id") int id, AcessoInputDto dto) {
+        try {
+            Acesso acesso = AcessoMapper.toModel(dto);
+            Acesso acessoEditado = acessoController.atualizarAcesso(id, acesso);
+            AcessoOutputDto acessoSaida = AcessoMapper.toDto(acessoEditado);
+            return Response.ok(acessoSaida).build();
+        } catch (RuntimeException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response delete(@PathParam("id") int id) {
+        try {
+            acessoController.deletarAcesso(id);
+            return Response.noContent().build();
+        } catch (RuntimeException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        }
+    }
+
+    @GET
+    @Path("/{userId}")
+    public Response listAllByUser(@PathParam("userId") int userId) {
+        try {
+            List<Acesso> acessoList = acessoController.listarAcessosPorUsuario(userId);
+            List<AcessoOutputDto> acessoSaida = new ArrayList<>();
+            for (Acesso acesso : acessoList) {
+                acessoSaida.add(AcessoMapper.toDto(acesso));
+            }
+            return Response.ok(acessoSaida).build();
+        } catch (RuntimeException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
+
 }
