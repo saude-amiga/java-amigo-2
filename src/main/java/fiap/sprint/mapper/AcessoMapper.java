af8794b9fc5e8b1435f1d6ddcf62b7bf855611c9
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-10-25 20:34:26 -0300

    feat(AcessoMapper): Conversor de acesso para objetoOutputDto

--- src/main/java/fiap/sprint/mapper/AcessoMapper.java
+++ src/main/java/fiap/sprint/mapper/AcessoMapper.java
@@ -1,4 +1,11 @@
 package fiap.sprint.mapper;
 
+import fiap.sprint.domain.model.Acesso;
+import fiap.sprint.dto.output.AcessoOutputDto;
+
 public class AcessoMapper {
+    public static AcessoOutputDto toDto(Acesso acessoCriado) {
+        return new AcessoOutputDto(acessoCriado.getId(), acessoCriado.getDataAcesso(),
+                acessoCriado.getIdPagina(), acessoCriado.getIdUsuario());
+    }
 }
